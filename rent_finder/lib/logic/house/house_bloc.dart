ba8403f3b044f180c7cc2cc780960a4c8f84ffcc
Longsans <70971402+Longsans@users.noreply.github.com>
2021-06-28 12:40:29 +0700

    smol change

--- rent_finder/lib/logic/house/house_bloc.dart
+++ rent_finder/lib/logic/house/house_bloc.dart
@@ -36,8 +36,7 @@ class HouseBloc extends Bloc<HouseEvent, HouseState> {
     try {
       var houses = await houseRepository.getHousesByLocation(
           event.quanHuyen, event.phuongXa);
-      yield HouseLoadSuccess(
-          houses: houses.where((element) => element.daGO != true).toList());
+      yield HouseLoadSuccess(houses: houses);
     } catch (err) {
       yield HouseLoadFailure();
     }
